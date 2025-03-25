ae2dde6287cc58b6682acde9276832e523b2ed86
ikasle <ikasle@DAM1-06.arangoya.local>
2025-03-25 14:04:11 +0100

    Version: 1.0.3

--- ejemploFinalGitHub/src/ejemploFinalGitHub/Main.java
+++ ejemploFinalGitHub/src/ejemploFinalGitHub/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i = 0; i < 1;i--) System.out.println("bobaina");
+		
+		System.out.println("Cara polla");
 	}
-
 }
